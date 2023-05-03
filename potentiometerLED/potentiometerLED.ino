751e2b0f5137e811b1a4f59b40178a9ab7642981
Jason <jasonli.0616@outlook.com>
2023-05-03 14:52:06 -0400

    Create potentiometerLED.ino

---
+++ potentiometerLED/potentiometerLED.ino
@@ -0,0 +1,34 @@
+const int LED_PINS[] = {10, 9, 8, 7, 6, 5};
+const int POTENTIOMETER_PIN = A0;
+
+const int AMOUNT_OF_LEDS = sizeof(LED_PINS) / sizeof(LED_PINS[0]);
+const int POTENTIOMETER_MAX_VALUE = 1023;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  for (int i = 0; i < AMOUNT_OF_LEDS; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
+
+  int onLEDs = (potentiometerValue / (double) POTENTIOMETER_MAX_VALUE) * AMOUNT_OF_LEDS;
+
+  Serial.println(onLEDs);
+
+  for (int i = 0; i < AMOUNT_OF_LEDS; i++) {
+    if (i < onLEDs)
+      digitalWrite(LED_PINS[i], HIGH);
+    else
+     digitalWrite(LED_PINS[i], LOW);
+  }
+
+}
