13bab1d2d37c2116efb3a9cd767c53ce5931ebf5
omairk8 <omairk8@gmail.com>
2023-05-04 14:21:15 -0400

    Create pins
    - IC D1, D2, D3, D0
    - Pushbutton

--- binary_number_counter/binary_number_counter.ino
+++ binary_number_counter/binary_number_counter.ino
@@ -1,9 +1,31 @@
+/**
+ * Binary number counter.
+ * 
+ * @author Jason Li
+ * @author Omair Khan
+ */
+
+
+const int BTN_PIN = 13;
+
+
+const int D1_PIN = 12;
+const int D2_PIN = 11
+const int D3_PIN = 10;
+const int D0_PIN = 9;
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  pinMode(BTN_PIN, INPUT);
+  pinMode(D1_PIN, OUTPUT);
+  pinMode(D2_PIN, OUTPUT);
+  pinMode(D3_PIN, OUTPUT);
+  pinMode(D0_PIN, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
