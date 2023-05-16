ac3c850cebb3cedfb87e52e3c5232db101017041
Jason <jasonli.0616@outlook.com>
2023-05-16 14:44:23 -0400

    Update elevator.ino

--- elevator/elevator.ino
+++ elevator/elevator.ino
@@ -7,25 +7,43 @@
 
 #include <Stepper.h>
 
-const int FLOOR_1_BTN = 8;
-const int FLOOR_2_BTN = 9;
-const int FLOOR_3_BTN = 12;
-const int FLOOR_4_BTN = 13;
+const int FLOOR_1_BTN = 2;
+const int FLOOR_2_BTN = 3;
+const int FLOOR_3_BTN = 4;
+const int FLOOR_4_BTN = 5;
 
 const int STEPS_PER_REVOLUTION = 200;
 
-Stepper myStepper = Stepper(STEPS_PER_REVOLUTION, 5, 6, 10, 11);
+Stepper myStepper = Stepper(STEPS_PER_REVOLUTION, 6, 9, 7, 10); // 1, 3, 2, 4
 
 // Elevator state
 int currentFloor = 4; // start at fourth floor
 const int TOTAL_FLOORS = 4;
+const int ONE_FLOOR_FACTOR = 1.13;
 
 void setup() {
   // Serial.begin(9600);
-  myStepper.setSpeed(200);
+  myStepper.setSpeed(60);
 }
 
 void loop() {
-  myStepper.step(STEPS_PER_REVOLUTION);
+  // Get floor input
+  int targetFloor = 0;
+
+  if (digitalRead(FLOOR_1_BTN) == HIGH)
+    targetFloor = 1;
+  else if (digitalRead(FLOOR_2_BTN) == HIGH)
+    targetFloor = 2;
+  else if (digitalRead(FLOOR_3_BTN) == HIGH)
+    targetFloor = 3;
+  else if (digitalRead(FLOOR_4_BTN) == HIGH)
+    targetFloor = 4;
+  
+  // Move to floor
+  if (targetFloor > 0) {
+    int moves = currentFloor - targetFloor;
+    myStepper.step(moves * (STEPS_PER_REVOLUTION * ONE_FLOOR_FACTOR));
+  }
+  
 
 }
