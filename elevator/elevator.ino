cc009ee8587005d6d2c154fe43975dea7ea60b20
omairk8 <omairk8@gmail.com>
2023-05-17 13:32:31 -0400

    Add LCD

--- elevator/elevator.ino
+++ elevator/elevator.ino
@@ -6,6 +6,7 @@
  */
 
 #include <Stepper.h>
+#include <LiquidCrystal.h>
 
 const int FLOOR_1_BTN = 2;
 const int FLOOR_2_BTN = 3;
@@ -13,6 +14,8 @@ const int FLOOR_3_BTN = 4;
 const int FLOOR_4_BTN = 5;
 
 const int STEPS_PER_REVOLUTION = 200;
+
+LiquidCrystal lcd(/* pins */);
 
 Stepper myStepper = Stepper(STEPS_PER_REVOLUTION, 6, 9, 7, 10); // 1, 3, 2, 4
 
@@ -24,12 +27,20 @@ const int ONE_FLOOR_FACTOR = 1.13;
 void setup() {
   // Serial.begin(9600);
   myStepper.setSpeed(60);
+  
+  lcd.begin(16, 2);
+ 
 }
 
 void loop() {
+
+  lcd.setCursor(0, 0);
+  lcd.print("ELEVATOR");
+  lcd.setCursor(0, 1);
+
   // Get floor input
   int targetFloor = 0;
-
+  
   if (digitalRead(FLOOR_1_BTN) == HIGH)
     targetFloor = 1;
   else if (digitalRead(FLOOR_2_BTN) == HIGH)
@@ -44,6 +55,8 @@ void loop() {
     int moves = currentFloor - targetFloor;
     myStepper.step(moves * (STEPS_PER_REVOLUTION * ONE_FLOOR_FACTOR));
   }
-  
+
+  lcd.setCursor(0, 1);
+  lcd.print(currentFloor);
 
 }
