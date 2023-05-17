450a9aff2d6836bb08f31504133eb92783e60878
Jason <jasonli.0616@outlook.com>
2023-05-17 14:41:52 -0400

    Kinda working elevator

--- elevator/elevator.ino
+++ elevator/elevator.ino
@@ -13,30 +13,35 @@ const int FLOOR_2_BTN = 3;
 const int FLOOR_3_BTN = 4;
 const int FLOOR_4_BTN = 5;
 
-const int STEPS_PER_REVOLUTION = 200;
+const int STEPS_PER_REVOLUTION = 512;
+const int MOVE_FOUR_FLOORS = 1250;
+const int MOVE_ONE_FLOOR = MOVE_FOUR_FLOORS / 4;
 
-LiquidCrystal lcd(/* pins */);
+// LiquidCrystal lcd = new LiquidCrystal(/* pins */);
 
 Stepper myStepper = Stepper(STEPS_PER_REVOLUTION, 6, 9, 7, 10); // 1, 3, 2, 4
 
 // Elevator state
-int currentFloor = 4; // start at fourth floor
+int currentFloor = 1; // start at fourth floor
 const int TOTAL_FLOORS = 4;
-const int ONE_FLOOR_FACTOR = 1.13;
+const int ONE_FLOOR_FACTOR = 1;
+
+// Button state
+bool buttonPressed = false;
 
 void setup() {
-  // Serial.begin(9600);
-  myStepper.setSpeed(60);
+  Serial.begin(9600);
+  myStepper.setSpeed(120);
   
-  lcd.begin(16, 2);
+  // lcd.begin(16, 2);
  
 }
 
 void loop() {
 
-  lcd.setCursor(0, 0);
-  lcd.print("ELEVATOR");
-  lcd.setCursor(0, 1);
+  // lcd.setCursor(0, 0);
+  // lcd.print("ELEVATOR");
+  // lcd.setCursor(0, 1);
 
   // Get floor input
   int targetFloor = 0;
@@ -50,13 +55,23 @@ void loop() {
   else if (digitalRead(FLOOR_4_BTN) == HIGH)
     targetFloor = 4;
   
-  // Move to floor
-  if (targetFloor > 0) {
-    int moves = currentFloor - targetFloor;
-    myStepper.step(moves * (STEPS_PER_REVOLUTION * ONE_FLOOR_FACTOR));
-  }
+  if (!buttonPressed && targetFloor != 0) {
+    buttonPressed = true;
 
-  lcd.setCursor(0, 1);
-  lcd.print(currentFloor);
+    // Move to floor
+    int moves = targetFloor - currentFloor;
+    Serial.println(moves);
+    myStepper.step(MOVE_ONE_FLOOR * moves);
+
+    currentFloor = targetFloor;
+
+  } else if (buttonPressed && targetFloor == 0) {
+    buttonPressed = false;
+  }
+  
+  // Move to floor
+
+  // lcd.setCursor(0, 1);
+  // lcd.print(currentFloor);
 
 }
