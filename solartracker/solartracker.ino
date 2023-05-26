840e92435be1d316e9b415c402e717442862f802
Jason <jasonli.0616@outlook.com>
2023-05-26 13:25:38 -0400

    Read values + move motor

--- solartracker/solartracker.ino
+++ solartracker/solartracker.ino
@@ -10,25 +10,44 @@ Servo servo;
 int photoresistorInput1 = 0;
 int photoresistorInput2 = 0;
 int photoresistorInput3 = 0;
+
+// The motor will use seconds as a unit for distance, as in
+// how much it can move at a constant velocity in the time given.
+const int VELOCITY = 40;
 int servoPosition = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
   servo.attach(SERVO_PIN);
+
+  servo.write(90);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  servoPosition = servo.read();
+  photoresistorInput1 = analogRead(PHOTORESISTOR_PIN_1) / 100;
+  // photoresistorInput2 = analogRead(PHOTORESISTOR_PIN_2);
+  photoresistorInput3 = analogRead(PHOTORESISTOR_PIN_3) / 100;
 
-  photoresistorInput1 = analogRead(PHOTORESISTOR_PIN_1);
-  photoresistorInput2 = analogRead(PHOTORESISTOR_PIN_2);
-  photoresistorInput3 = analogRead(PHOTORESISTOR_PIN_3);
+  Serial.print(photoresistorInput1);
+  Serial.print(" ");
+  Serial.print(photoresistorInput3);
+  Serial.print("\n");
+}
 
-  Serial.println(servoPosition);
+/**
+ * Spin at a constant velocity for x seconds,
+ * representing the distance.
+ */
+void spinDistance(int distance) {
+  int directionFactor = (distance < 0) ? -1 : 1;
+  servo.write(90 + (VELOCITY * directionFactor));
 
+  delay(distance);
+  servo.write(90);
+
+  servoPosition += distance;
 }
