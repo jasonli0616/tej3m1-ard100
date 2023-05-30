0ee8e1e0d75240ba9fc5a423ebfdb4bc9c7f79a9
Jason <jasonli.0616@outlook.com>
2023-05-30 14:31:58 -0400

    Complete stage 2

--- thermostat/thermostat.ino
+++ thermostat/thermostat.ino
@@ -3,46 +3,86 @@
 #define LED_G_PIN A3
 #define LED_B_PIN A2
 
+#define COOL_MOTOR 3
+#define HEAT_MOTOR 2
+
 #define LED_OFF 0
 #define LED_MAX 255
 
 const int TEMPERATURE_COMFORTABLE_RANGE[] = {20, 23}; // {min, max}
 
+// variables
 float temperature;
+int state; // 0=off, 1=cold, 2=hot
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
   pinMode(TMP36_PIN, INPUT);
+
   pinMode(LED_R_PIN, OUTPUT);
   pinMode(LED_G_PIN, OUTPUT);
   pinMode(LED_B_PIN, OUTPUT);
+  
+  pinMode(COOL_MOTOR, OUTPUT);
+  pinMode(HEAT_MOTOR, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  temperature = getTemperature();
-  Serial.println(temperature);
 
-  setLEDs(temperature);
+  // Get temperature
+  temperature = getTemperature();
+  state = getTemperatureState(temperature);
+
+  // Set
+  setLEDs();
+  determineMotorOutput();
 }
 
-void setLEDs(float temperature) {
+void determineMotorOutput() {
+  if (state == 1) {
+    digitalWrite(COOL_MOTOR, LOW);
+    digitalWrite(HEAT_MOTOR, HIGH);
+  } else if (state == 2) {
+    digitalWrite(COOL_MOTOR, HIGH);
+    digitalWrite(HEAT_MOTOR, LOW);
+  } else {
+    digitalWrite(COOL_MOTOR, LOW);
+    digitalWrite(HEAT_MOTOR, LOW);
+  }
+}
 
-  analogWrite(LED_R_PIN, LED_OFF);
-  analogWrite(LED_G_PIN, LED_OFF);
-  analogWrite(LED_B_PIN, LED_OFF);
+void setLEDs() {
 
   // Turn on LEDs based on temperature
-  if (temperature < TEMPERATURE_COMFORTABLE_RANGE[0]) {
+  if (state == 1) {
+    analogWrite(LED_R_PIN, LED_OFF);
+    analogWrite(LED_G_PIN, LED_OFF);
     analogWrite(LED_B_PIN, LED_MAX);
-  } else if (temperature > TEMPERATURE_COMFORTABLE_RANGE[1]) {
+  } else if (state == 2) {
     analogWrite(LED_R_PIN, LED_MAX);
+    analogWrite(LED_G_PIN, LED_OFF);
+    analogWrite(LED_B_PIN, LED_OFF);
   } else {
+    analogWrite(LED_R_PIN, LED_OFF);
     analogWrite(LED_G_PIN, LED_MAX);
+    analogWrite(LED_B_PIN, LED_OFF);
   }
+}
+
+/**
+ * 0=off, 1=cold, 2=hot
+ */
+int getTemperatureState(float temperature) {
+  if (temperature < TEMPERATURE_COMFORTABLE_RANGE[0]) {
+    return 1;
+  } else if (temperature > TEMPERATURE_COMFORTABLE_RANGE[1]) {
+    return 2;
+  }
+  return 0;
 }
 
 float getTemperature() {
